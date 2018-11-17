9eae55d03644701c9edd94879efc7e5d69368b54
elijah <43258530+DrProgrammedChild@users.noreply.github.com>
2018-11-17 00:26:55 -0800

    Add files via upload

---
+++ Util/Database/database.js
@@ -0,0 +1,62 @@
+var dbjson = require("./database.json");
+var fs = require("fs");
+
+class User{
+	constructor(options){
+		this.name = options.name;
+		this.id = options.id;
+		this.tc = options.tc;
+		this.bc = options.bc;
+		this.tfc = options.tfc;
+		this.nbc = options.nbc;
+	}
+	giveTc(amount){
+		this.tc += amount;
+		dbjson["id-" + this.id] = {
+			name: this.name,
+			id: this.id,
+			tc: this.tc,
+			bc: this.bc,
+			tfc: this.tfc,
+			nbc: this.nbc
+		};
+		fs.writeFile("./Util/Database/database.json",JSON.stringify(dbjson),console.log);
+	}
+	takeTc(amount){
+		this.tc -= amount;
+		dbjson["id-" + this.id] = {
+			name: this.name,
+			id: this.id,
+			tc: this.tc,
+			bc: this.bc,
+			tfc: this.tfc,
+			nbc: this.nbc
+		};
+		fs.writeFile("./Util/Database/database.json",JSON.stringify(dbjson),console.log);
+	}
+}
+
+module.exports = {
+	getUser: function(user){
+		return new Promise((resolve,reject) => {
+			let dbentry = dbjson["id-" + user.id];
+			if(dbentry){
+				let usere = new User(dbentry);
+				resolve(usere);
+			} else{
+				dbentry = {
+					name: user.user.username,
+					id: user.id,
+					tc: 10,
+					bc: 0,
+					tfc: 0,
+					nbc: 0
+				}
+				dbjson["id-" + user.id] = dbentry;
+				fs.writeFile("./Util/Database/database.json",dbjson,console.log);
+				let usere = new User(dbentry);
+				resolve(usere);
+			}
+		});
+	}
+};
