12fc719a3b16ac66511cd5a82ec0568aee820998
elijah <43258530+DrProgrammedChild@users.noreply.github.com>
2018-11-17 17:51:04 -0800

    Update commands.js

--- Util/commands.js
+++ Util/commands.js
@@ -1,5 +1,11 @@
 var database = require("./Database/database.js");
 var shoputil = require("./shop.js");
+var musicutil = require("./music.js");
+var request = require("request");
+
+function random(min,max) {
+	return Math.floor(Math.random()*(max-min+1))+min;
+}
 
 module.exports = {
 	commands: [
@@ -231,6 +237,145 @@ module.exports = {
 			args: [
 				{name: "itemnum", required: true}
 			],
+			rolesallowed: false
+		},
+		{
+			name: "source",
+			category: "Basic",
+			description: "Some of the sexy programming behind the bot",
+			execute: function(client,msg,args){
+				msg.channel.send("I'll show you my source code if you show me yours ;)\nhttps://github.com/DrProgrammedChild/toxic-bot/");
+			},
+			args: false,
+			rolesallowed: false
+		},
+		{
+			name: "play",
+			category: "Fun",
+			description: "Plays a song",
+			execute: function(client,msg,args){
+				if(musicutil.playing){
+					musicutil.queueAdd(args.join(" "),msg.author.username)
+						.then(song => {
+							msg.channel.send("Added **" + song.name + "** to queue.");
+						})
+						.catch(console.log);
+				} else{
+					if(msg.member.voiceChannel){
+						musicutil.play(args.join(" "),msg.author.username,msg.member.voiceChannel,msg.channel);
+					} else{
+						msg.channel.send(":no_entry_sign: Error: You must be in a voice channel to use this command");
+					}
+				}
+			},
+			args: [
+				{name: "song", required: true}
+			],
+			rolesallowed: false
+		},
+		{
+			name: "pause",
+			category: "Fun",
+			description: "Pauses current song",
+			execute: function(client,msg,args){
+				musicutil.pause(msg);
+			},
+			args: false,
+			rolesallowed: false
+		},
+		{
+			name: "resume",
+			category: "Fun",
+			description: "Resumes current song",
+			execute: function(client,msg,args){
+				musicutil.resume(msg);
+			},
+			args: false,
+			rolesallowed: false
+		},
+		{
+			name: "disconnect",
+			category: "Fun",
+			description: "Disconnects from current voice channel",
+			execute: function(client,msg,args){
+				musicutil.disconnect(msg);
+			},
+			args: false,
+			rolesallowed: false
+		},
+		{
+			name: "queue",
+			category: "Fun",
+			description: "Displays queue",
+			execute: function(client,msg,args){
+				musicutil.getQueue()
+					.then(queue => {
+						let embed = {};
+						embed.title = "Queue";
+						embed.fields = [];
+						embed.footer = {text: "Do -queue for queue"};
+						for(let i = 0; i < queue.length; i++){
+							let queueitem = queue[i];
+							embed.fields.push({
+								name: (i+1) + ") " + queueitem.name,
+								value: "requested by **" + queueitem.requester + "**"
+							});
+						}
+						msg.channel.send({embed: embed});
+					})
+					.catch(console.log);
+			},
+			args: false,
+			rolesallowed: false
+		},
+		{
+			name: "skip",
+			category: "Fun",
+			description: "Skips current song in queue",
+			execute: function(client,msg,args){
+				musicutil.skip(msg);
+			},
+			args: false,
+			rolesallowed: false
+		},
+		{
+			name: "meme",
+			category: "Fun",
+			description: "Gives you a MEME",
+			execute: function(client,msg,args){
+				let url;
+				if(args[0] == "true"){
+					url = "https://www.reddit.com/r/deepfriedmemes/new.json?sort=new";
+				} else{
+					url = "https://www.reddit.com/r/dankmemes/new.json?sort=new";
+				}
+				request(url,(err,res,body) => {
+					let json = JSON.parse(body);
+					let posts = json.data.children;
+					let post = posts[random(0,posts.length)];
+					while(post == undefined){
+						post = posts[random(0,posts.length)];
+					}
+					msg.channel.send(post.data.title,{
+						files: [
+							post.data.url
+						]
+					});
+				});
+			},
+			args: [
+				{name: "deepfried", required: false}
+			],
+			rolesallowed: false
+		},
+		{
+			name: "version",
+			category: "Basic",
+			description: "Version of the bot (and some legal stuff)",
+			execute: function(client,msg,args){
+				msg.channel.send("Version v1.0.0 (Testing edition)\nMade by _MFSec in assosiation with the Toxic cult\nCopyright 2018");
+			},
+			args: false,
 			rolesallowed: false
 		}
 	]
