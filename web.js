47b01cb1e7c355cb58416ca0c8390cc855c8c4a8
elijah <43258530+DrProgrammedChild@users.noreply.github.com>
2018-11-17 23:55:35 -0800

    Update web.js

--- web.js
+++ web.js
@@ -1,1 +1,18 @@
-//For later use
+//Made by _programmeKid
+
+//Variables
+var express = require("express");
+var bodyParser = require("body-parser");
+var app = express();
+
+//Express routes
+var urlencodedParser = bodyParser.urlencoded({extended: false});
+app.use(express.static("client"));
+
+app.get("/",(req,res) => {
+	res.sendFile("./client/index.html");
+});
+
+//Start server
+app.listen(process.env.PORT || 8081);
+console.log("Listening on port " + (process.env.PORT || 8081));
